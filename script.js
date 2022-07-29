f0a776f2475dee1ddb9ddf3bc834e801ad8094e5
Piotr Jemielita <persolanstereox@gmail.com>
2022-07-29 19:42:00 +0200

    first funcionality

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 'use strict'
+const bill = document.getElementById('inputForBill');
+const customTip = document.getElementById('customTip');
+const percantageBtns = document.querySelectorAll('.btnBillPercantage');
+const numberOfPeople = document.getElementById('inputNumberOfPeople');
+const tipAmount = document.querySelector('.tipForOnePerson');
+const total = document.querySelector('.totalForOnePerson');
+const tip = 0.05;
+bill.value = 0;
+numberOfPeople.value = 0;
+customTip.value = 0;
+
+for(let i =0; i < percantageBtns.length; i++) {
+    percantageBtns[i].addEventListener('click', function() {
+        percantageBtns[i].value = percantageBtns[i-1].value + percantageBtns[i-2].value
+    })
+}
+console.log(percantageBtns[2].value);
+
+function calculateTip() {
+
+}
